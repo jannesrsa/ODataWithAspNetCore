17700a09bd735648f6b22fe8bf15526665c477ed
Jannes Coetzee <jannesrsa@gmail.com>
2019-01-17 15:16:19 +0200

    Initial Repro https://damienbod.com/2018/10/12/odata-with-asp-net-core/
    Setting up the Database

---
+++ src/EFGetStarted.AspNetCore.ExistingDb/EFGetStarted.AspNetCore.ExistingDb/Models/PhoneNumberType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFGetStarted.AspNetCore.ExistingDb.Models
+{
+    public partial class PhoneNumberType
+    {
+        public PhoneNumberType()
+        {
+            PersonPhone = new HashSet<PersonPhone>();
+        }
+
+        public int PhoneNumberTypeId { get; set; }
+        public string Name { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public ICollection<PersonPhone> PersonPhone { get; set; }
+    }
+}
